90e44d5ef85a0316a93f5a69d724a4659dec2b66
TwiDev <twidev5@gmail.com>
2023-06-16 13:55:36 +0200

    add nms packet factory

--- src/main/java/ch/twidev/spectral/packet/version/PacketsV1_8.java
+++ src/main/java/ch/twidev/spectral/packet/version/PacketsV1_8.java
@@ -1,43 +1,38 @@
 package ch.twidev.spectral.packet.version;
 
+import ch.twidev.spectral.SpectralDamage;
 import ch.twidev.spectral.packet.IPackets;
 import ch.twidev.spectral.utils.StringUtils;
 import net.minecraft.server.v1_8_R3.*;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.craftbukkit.v1_8_R3.CraftWorld;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
+import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.plugin.Plugin;
 
 public class PacketsV1_8 implements IPackets {
     @Override
     public int spawnHologram(Player player, Location location, double damage, Plugin plugin) {
+        Bukkit.getScheduler().runTaskAsynchronously(SpectralDamage.get(), () -> {
+            PlayerConnection connection = ((CraftPlayer) player).getHandle().playerConnection;
+            World mcWorld = ((CraftWorld) player.getWorld()).getHandle();
 
-        PlayerConnection connection = ((CraftPlayer) player).getHandle().playerConnection;
-        World mcWorld = ((CraftWorld) location.getWorld()).getHandle();
+            EntityArmorStand armorStand = new EntityArmorStand(mcWorld, location.getX(), location.getY(), location.getZ()) {{
+                setCustomName(StringUtils.getDamageFormat(damage));
+                setCustomNameVisible(true);
+            }};
 
-        EntityArmorStand armorStand = new EntityArmorStand(mcWorld) {{
-            setLocation(
-                    location.getX(),
-                    location.getY(),
-                    location.getZ(),
-                    location.getYaw(),
-                    location.getPitch());
-            setInvisible(false);
-            setSmall(true);
-            setGravity(false);
-            setCustomName(StringUtils.getDamageFormat(damage));
-            setCustomNameVisible(true);
-        }};
+            int armorStandID = armorStand.getId();
 
-        int armorStandID = armorStand.getId();
+            PacketPlayOutSpawnEntityLiving packet = new PacketPlayOutSpawnEntityLiving(armorStand);
+            PacketPlayOutEntityMetadata metadata = new PacketPlayOutEntityMetadata(armorStandID, armorStand.getDataWatcher(), true);
+            connection.sendPacket(packet);
+            connection.sendPacket(metadata);
+        });
 
-        PacketPlayOutSpawnEntityLiving packet = new PacketPlayOutSpawnEntityLiving(armorStand);
-        PacketPlayOutEntityMetadata metadata = new PacketPlayOutEntityMetadata(armorStand.getId(), armorStand.getDataWatcher(), true);
-        connection.sendPacket(packet);
-        connection.sendPacket(metadata);
-
-        return armorStandID;
+        return -1;
 
     }
 
--- src/main/java/ch/twidev/spectral/utils/StringUtils.java
+++ src/main/java/ch/twidev/spectral/utils/StringUtils.java
@@ -6,7 +6,7 @@ import ch.twidev.spectral.config.ConfigVars;
 public class StringUtils {
 
     public static String getDamageFormat(double damage){
-        return ConfigManager.CONFIG_VALUES.get(ConfigVars.HOLOGRAM_DAMAGE_FORMAT).asString().replaceAll("%damage%", String.valueOf(Math.round(damage)));
+        return ConfigManager.CONFIG_VALUES.get(ConfigVars.HOLOGRAM_DAMAGE_FORMAT).asString().replaceAll("&","§").replaceAll("%damage%", String.valueOf(Math.round(damage)));
     }
 
 }
