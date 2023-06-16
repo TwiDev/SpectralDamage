36b35f936eadaf0c5a3663d22bdeac401854f4dd
TwiDev <twidev5@gmail.com>
2023-06-16 16:47:09 +0200

    remove old src folder

--- src/main/java/ch/twidev/spectral/packet/UnsupportedVersionException.java
+++
@@ -1,8 +0,0 @@
-package ch.twidev.spectral.packet;
-
-public class UnsupportedVersionException extends RuntimeException{
-
-    public UnsupportedVersionException() {
-        super("The version of your spigot instance is not supported by this plugin");
-    }
-}
