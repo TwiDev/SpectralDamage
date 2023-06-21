46a76173958d4181ce46b872f1b5fa0149def2f7
TwiDev <twidev5@gmail.com>
2023-06-21 12:14:05 +0200

    add javadoc for api

--- api/src/main/java/ch/twidev/spectraldamage/api/SpectralDamage.java
+++ api/src/main/java/ch/twidev/spectraldamage/api/SpectralDamage.java
@@ -21,32 +21,119 @@ public abstract class SpectralDamage {
         return plugin;
     }
 
+    /**
+     * Spawn a damage indicator visible only to a player
+     *
+     * @param player location of the damager
+     * @param location location of the damage indicator
+     * @param critical is damage a critical (to format the hologram)
+     * @param damage point of damage to show in the hologram
+     * @param falling is hologram falling to the ground (animation configurable)
+     * @return Damage indicator hologram (ArmorStand entity) (Packet entity)
+     */
+
     public abstract Entity spawnDamageIndicator(Player player, Location location, boolean critical, int damage, boolean falling);
 
+    /**
+     * Spawn a damage indicator visible only to a player
+     *
+     * @param player location of the damager
+     * @param location location of the damage indicator
+     * @param critical is damage a critical (to format the hologram)
+     * @param damage point of damage to show in the hologram
+     * @return Damage indicator hologram (ArmorStand entity) (Packet entity)
+     */
     public abstract Entity spawnDamageIndicator(Player player, Location location, boolean critical, int damage);
 
+    /**
+     * shows a damage indicator visible to all players who have not disabled damage indicators
+     *
+     * @param location location of the damage indicator
+     * @param critical is damage a critical (to format the hologram)
+     * @param damage point of damage to show in the hologram
+     * @param falling is hologram falling to the ground (animation configurable)
+     * @return Damage indicator hologram (ArmorStand entity)
+     */
     public abstract Entity spawnDamageIndicator(Location location, boolean critical, int damage, boolean falling);
 
+    /**
+     * shows a damage indicator visible to all players who have not disabled damage indicators
+     *
+     * @param location location of the damage indicator
+     * @param critical is damage a critical (to format the hologram)
+     * @param damage point of damage to show in the hologram
+     * @return Damage indicator hologram (ArmorStand entity)
+     */
+
     public abstract Entity spawnDamageIndicator(Location location, boolean critical, int damage);
+
+
+    /**
+     * Spawn a damage indicator visible only to a player
+     *
+     * @param player location of the damager
+     * @param location location of the damage indicator
+     * @param damage point of damage to show in the hologram
+     * @param falling is hologram falling to the ground (animation configurable)
+     * @return Damage indicator hologram (ArmorStand entity) (Packet entity)
+     */
 
     public Entity spawnDamageIndicator(Player player, Location location, int damage, boolean falling) {
         return this.spawnDamageIndicator(player, location, false, damage, falling);
     }
 
+
+    /**
+     * Spawn a damage indicator visible only to a player
+     *
+     * @param player location of the damager
+     * @param location location of the damage indicator
+     * @param damage point of damage to show in the hologram
+     * @return Damage indicator hologram (ArmorStand entity) (Packet entity)
+     */
+
     public Entity spawnDamageIndicator(Player player, Location location, int damage) {
         return this.spawnDamageIndicator(player, location, false, damage);
     }
 
+    /**
+     * shows a damage indicator visible to all players who have not disabled damage indicators
+     *
+     * @param location location of the damage indicator
+     * @param damage point of damage to show in the hologram
+     * @param falling is hologram falling to the ground (animation configurable)
+     * @return Damage indicator hologram (ArmorStand entity)
+     */
     public Entity spawnDamageIndicator(Location location, int damage, boolean falling) {
         return this.spawnDamageIndicator(location, false, damage, falling);
     }
+
+    /**
+     * shows a damage indicator visible to all players who have not disabled damage indicators
+     *
+     * @param location location of the damage indicator
+     * @param damage point of damage to show in the hologram
+     * @return Damage indicator hologram (ArmorStand entity)
+     */
 
     public Entity spawnDamageIndicator(Location location, int damage) {
         return this.spawnDamageIndicator(location, false, damage);
     }
 
+    /**
+     * Enable damage indicators for a specific player (they are enabled by default)
+     * @param player player
+     * @return if the request was successful
+     */
     public abstract boolean enableDamageIndicators(Player player);
 
+    /**
+     * disable damage indicators for a specific player
+     * (it will no longer make holograms only if they are visible only to the damager)
+     *
+     * @param player player
+     * @return if the request was successful
+     */
     public abstract boolean disableDamageIndicators(Player player);
 
     public static SpectralDamage getInstance() {
