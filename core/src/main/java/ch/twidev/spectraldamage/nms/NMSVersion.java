b430ae857445da495f807ea0235e3a54c9b5015c
twidev <twidev5@gmail.com>
2024-08-16 22:23:54 +0200

    add 1.21 Support

--- core/src/main/java/ch/twidev/spectraldamage/nms/NMSVersion.java
+++ core/src/main/java/ch/twidev/spectraldamage/nms/NMSVersion.java
@@ -34,13 +34,28 @@ public enum NMSVersion {
     /* 1.20 - 1.20.1   */ v1_20_R1(() -> new ch.twidev.spectraldamage.nms.v1_20_R1.PacketsV1_20_R1()),
     /* 1.20.2 - 1.20.3 */ v1_20_R2(() -> new ch.twidev.spectraldamage.nms.v1_20_R2.PacketsV1_20_R2()),
     /* 1.20.3 - 1.20.4 */ v1_20_R3(() -> new ch.twidev.spectraldamage.nms.v1_20_R3.PacketsV1_20_R3()),
-    /* 1.20.5 - ???    */ v1_20_R4(() -> new ch.twidev.spectraldamage.nms.v1_20_R4.PacketsV1_20_R4()),
+    /* 1.20.5 - 1.20.6 */ v1_20_R4(() -> new ch.twidev.spectraldamage.nms.v1_20_R4.PacketsV1_20_R4()),
+    /* 1.21   - X      */ v1_21_R1(() -> new ch.twidev.spectraldamage.nms.v1_21_R1.PacketsV1_21_R1()),
     /* Other versions  */ UNKNOWN(NMSManagerFactory.unknownVersion());
 
-    private static final NMSVersion CURRENT_VERSION;
+    private static NMSVersion CURRENT_VERSION;
 
     static {
-        CURRENT_VERSION = detectCurrentVersion();
+        CURRENT_VERSION = detectCurrentVersion(Bukkit.getServer().getClass().getPackage().getName());
+
+        System.out.println(Bukkit.getServer().getBukkitVersion());
+        System.out.println(Bukkit.getServer().getClass().getPackage().getName());
+
+        if(CURRENT_VERSION == UNKNOWN) {
+            Matcher matcher = Pattern.compile("\\d+.\\d+.\\d+").matcher(Bukkit.getServer().getBukkitVersion());
+            if (!matcher.find()) {
+                CURRENT_VERSION = UNKNOWN;
+            }
+
+            System.out.println(matcher.group());
+
+            CURRENT_VERSION = detectCurrentVersion(matcher.group());
+        }
     }
 
     final NMSManagerFactory iPackets;
@@ -57,8 +72,31 @@ public enum NMSVersion {
         return iPackets;
     }
 
-    private static NMSVersion detectCurrentVersion() {
-        Matcher matcher = Pattern.compile("v\\d+_\\d+_R\\d+").matcher(Bukkit.getServer().getClass().getPackage().getName());
+    private static NMSVersion detectCurrentVersion(String name) {
+        String bukkitVersion = Bukkit.getServer().getBukkitVersion();
+        int    majorVersion  = Integer.parseInt(bukkitVersion.split("[.-]")[1]);
+        if (majorVersion >= 20) {
+            switch (bukkitVersion) {
+                case "1.20-R0.1-SNAPSHOT":
+                case "1.20.1-R0.1-SNAPSHOT":
+                    return v1_20_R1;
+                case "1.20.2-R0.1-SNAPSHOT":
+                    return v1_20_R2;
+                case "1.20.3-R0.1-SNAPSHOT":
+                case "1.20.4-R0.1-SNAPSHOT":
+                    return v1_20_R3;
+                case "1.20.5-R0.1-SNAPSHOT":
+                case "1.20.6-R0.1-SNAPSHOT":
+                    return v1_20_R4;
+                case "1.21-R0.1-SNAPSHOT":
+                    return v1_21_R1;
+                default:
+                    return UNKNOWN;
+            }
+        }
+
+        Matcher matcher =
+                Pattern.compile("v\\d+_\\d+_R\\d+").matcher(Bukkit.getServer().getClass().getPackage().getName());
         if (!matcher.find()) {
             return UNKNOWN;
         }
