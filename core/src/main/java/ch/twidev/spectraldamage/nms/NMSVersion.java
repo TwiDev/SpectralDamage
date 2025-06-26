8ef34d470275b0bffa3e0b1ee2b2a2ff3067a00c
twidev <twidev5@gmail.com>
2025-06-26 22:15:55 +0200

    add 1.21.6 support (v 2.7.2)

--- core/src/main/java/ch/twidev/spectraldamage/nms/NMSVersion.java
+++ core/src/main/java/ch/twidev/spectraldamage/nms/NMSVersion.java
@@ -38,7 +38,8 @@ public enum NMSVersion {
     /* 1.21   - 1.21.1 */ v1_21_R1(() -> new ch.twidev.spectraldamage.nms.v1_21_R1.PacketsV1_21_R1()),
     /* 1.21.2 - 1.21.3 */ v1_21_R2(() -> new ch.twidev.spectraldamage.nms.v1_21_R2.PacketsV1_21_R2()),
     /* 1.21.3 - 1.21.4 */ v1_21_R3(() -> new ch.twidev.spectraldamage.nms.v1_21_R3.PacketsV1_21_R3()),
-    /* 1.21.3 - 1.21.4 */ v1_21_R4(() -> new ch.twidev.spectraldamage.nms.v1_21_R4.PacketsV1_21_R4()),
+    /* 1.21.5 - 1.21.5 */ v1_21_R4(() -> new ch.twidev.spectraldamage.nms.v1_21_R4.PacketsV1_21_R4()),
+    /* 1.21.6 - 1.21.? */ v1_21_R5(() -> new ch.twidev.spectraldamage.nms.v1_21_R5.PacketsV1_21_R5()),
     /* Other versions  */ UNKNOWN(NMSManagerFactory.unknownVersion());
 
     private static NMSVersion CURRENT_VERSION;
@@ -87,6 +88,8 @@ public enum NMSVersion {
                     return v1_21_R3;
                 case "1.21.5-R0.1-SNAPSHOT":
                     return v1_21_R4;
+                case "1.21.6-R0.1-SNAPSHOT":
+                    return v1_21_R5;
                 default:
                     return UNKNOWN;
             }
